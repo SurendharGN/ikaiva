8fe6f47bac959697e6d9e20f6342e83b30158eb7
SurendharGN <work.surendhar@gmail.com>
2024-04-10 13:28:39 +0530

    customer support

--- pages/CustomerSupport.jsx
+++ pages/CustomerSupport.jsx
@@ -3,6 +3,10 @@ import React from 'react'
 const CustomerSupport = () => {
   return (
     <div>
+      <p>Address</p>
+      <p>#209, Shirdi Sai Concord Heights</p>
+      <p>Bangalore</p>
+      <p>India</p>
       Contact us at ikaiva.co@gmail.com for any queries related to the service and the subscription.
 Contact no: +91 7204909498
 
